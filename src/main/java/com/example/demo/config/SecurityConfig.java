8a6d4be6e1d7e1c9beda292b75fb0d7c5478de3e
mariosaez <mariosaezvilluendas@gmail.com>
2024-05-31 21:26:08 +0200

    feature/addSwagger

---
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .formLogin(withDefaults());
+
+        return http.build();
+    }
+}
