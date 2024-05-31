72dae733d0db054a7dc66e24e20657c325ea0b73
mariosaez <mariosaezvilluendas@gmail.com>
2024-05-31 20:03:08 +0200

    feature/createRepositories

---
+++ src/main/java/com/example/demo/repositories/TaskRepository.java
@@ -0,0 +1,13 @@
+package com.example.demo.repositories;
+
+import com.example.demo.models.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface TaskRepository extends JpaRepository<Task, UUID> {
+
+    Optional<Task> findByTitle(String title);
+
+}
